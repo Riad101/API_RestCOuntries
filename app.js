588a18cf4d1f2088c35f9e5bd1dc517c4df220ea
smriad <skmdriad@gmail.com>
2021-04-01 00:30:08 +0800

    design added

--- app.js
+++ app.js
@@ -4,22 +4,36 @@ fetch("https://restcountries.eu/rest/v2/all")
 
 const displayCountries = countries => {
     const countriesDiv = document.getElementById("countries")
-    for (let i = 0; i < countries.length; i++) {
-        const country = countries[i];
+
+    countries.forEach(country => {
         const countryDiv = document.createElement("div");
-        // const countryName = document.createElement("H2");
-        // countryName.innerText = country.name;
-        // const countryCapital = document.createElement("p");
-        // countryCapital.innerText = country.capital;
-        // countryDiv.appendChild(countryName);
-        // countryDiv.appendChild(countryCapital);
-        // countriesDiv.appendChild(countryDiv);  
-        
+        countryDiv.className = "country";
         const countryInfo = `
-            <h2>${country.name}</h2>
+            <h3 class="country-name">${country.name}</h3>
             <p>${country.capital}</p>
-            `
+        `;
         countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
-    }
+    });
+
+    // for (let i = 0; i < countries.length; i++) {
+    //     const country = countries[i];
+    //     const countryDiv = document.createElement("div");
+    //     countryDiv.className = "country";
+
+    //     // const countryName = document.createElement("H2");
+    //     // countryName.innerText = country.name;
+    //     // const countryCapital = document.createElement("p");
+    //     // countryCapital.innerText = country.capital;
+    //     // countryDiv.appendChild(countryName);
+    //     // countryDiv.appendChild(countryCapital);
+    //     // countriesDiv.appendChild(countryDiv);  
+        
+    //     const countryInfo = `
+    //         <h2 class="country-name">${country.name}</h2>
+    //         <p>${country.capital}</p>
+    //         `
+    //     countryDiv.innerHTML = countryInfo;
+    //     countriesDiv.appendChild(countryDiv);
+    // };
 }
